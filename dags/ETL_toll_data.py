4783370337dcc5742f01303f57d3f8c1f799476f
Mario Loera <mario.loera@trustly.com>
2022-02-25 12:39:39 +0100

    rename target varoables for reuse

--- dags/ETL_toll_data.py
+++ dags/ETL_toll_data.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 
 
@@ -8,11 +8,11 @@ from airflow.utils.dates import days_ago
 default_args = {
     "owner": "Mario Loera",
     "start_date": days_ago(0),
-    # "email": ["marioloera@somemail.com"],
-    # "email_on_failure": True,
-    # "email_on_retry": True,
-    # "retries": 1,
-    # "retry_delay": timedelta(minutes=5),
+    "email": ["marioloera@somemail.com"],
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 # Task 1.2 - Define the DAG
@@ -25,32 +25,35 @@ dag = DAG(
 
 # Task 1.3 - Create a task to unzip data
 SRC = "/tmp/project/airflow/dags/finalassignment/tolldata.tgz"
-TARGET = "/tmp/project/airflow/dags/finalassignment/staging"
+STAGING = "/tmp/project/airflow/dags/finalassignment/staging"
+CMD = f"tar zxvf {SRC} -C {STAGING}"
 unzip_data = BashOperator(
     task_id="unzip_data",
-    bash_command=f"tar zxvf {SRC} -C {TARGET}",
+    bash_command=CMD,
     dag=dag,
 )
+
 # airflow tasks test ETL_toll_data unzip_data 20220224
 
 # Task 1.4 - Create a task to extract data from csv file
 SRC = "/tmp/project/airflow/dags/finalassignment/staging/vehicle-data.csv"
-TARGET = "/tmp/project/airflow/dags/finalassignment/staging/csv_data.csv"
+CSV_DATA = "/tmp/project/airflow/dags/finalassignment/staging/csv_data.csv"
+CMD = f"cut -d',' -f1-4 {SRC} > {CSV_DATA}"
 extract_data_from_csv = BashOperator(
     task_id="extract_data_from_csv",
-    bash_command=f'cut -d"," -f1-4 {SRC} > {TARGET}',
+    bash_command=CMD,
     dag=dag,
 )
 
 # airflow tasks test ETL_toll_data extract_data_from_csv 20220224
 
-
 # Task 1.5 - Create a task to extract data from tsv file
-SRC="/tmp/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv"
-TARGET="/tmp/project/airflow/dags/finalassignment/staging/tsv_data.csv"
+SRC = "/tmp/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv"
+TSV_DATA = "/tmp/project/airflow/dags/finalassignment/staging/tsv_data.csv"
+CMD = f"tr $'\t' ',' < {SRC} | cut -d',' -f5-7 > {TSV_DATA}"
 extract_data_from_tsv = BashOperator(
     task_id="extract_data_from_tsv",
-    bash_command=f"tr $'\t' ',' < {SRC} | cut -d',' -f5-7 > {TARGET}",
+    bash_command=CMD,
     dag=dag,
 )
 
@@ -58,16 +61,27 @@ extract_data_from_tsv = BashOperator(
 
 # Task 1.6 - Create a task to extract data from fixed width file
 SRC = "/tmp/project/airflow/dags/finalassignment/staging/payment-data.txt"
-TARGET = "/tmp/project/airflow/dags/finalassignment/staging/fixed_width_data.csv"
-cmd = f"cut -c 59-67 {SRC} | tr ' ' ',' > {TARGET}"
+FIXED_WITH_DATA = "/tmp/project/airflow/dags/finalassignment/staging/fixed_width_data.csv"
+CMD = f"cut -c 59-67 {SRC} | tr ' ' ',' > {FIXED_WITH_DATA}"
 extract_data_from_fixed_width = BashOperator(
     task_id="extract_data_from_fixed_width",
-    bash_command=cmd,
+    bash_command=CMD,
     dag=dag,
 )
 
 # airflow tasks test ETL_toll_data extract_data_from_fixed_width 20220224
 
 
+end_task = BashOperator(
+    task_id="end_task",
+    bash_command="echo end",
+    dag=dag,
+)
+
+
 # pipeline
-unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width
+unzip_data >> [
+    extract_data_from_csv,
+    extract_data_from_tsv,
+    extract_data_from_fixed_width,
+] >> end_task
