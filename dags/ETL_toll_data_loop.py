ab98d2adcba3b01ae5d0f085b75d67c857f9f80d
Mario Loera <mario.loera@trustly.com>
2022-02-26 19:49:23 +0100

    use the function get_task in loop dag

--- dags/ETL_toll_data_loop.py
+++ dags/ETL_toll_data_loop.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
+from sqlalchemy import false
 
 DIR = "/tmp/project/airflow/dags/finalassignment/staging/"
 ENV_VAR = {
@@ -37,32 +38,36 @@ dag = DAG(
     description="Apache Airflow Final Assignment",
 )
 
-# Extra make dir
-CMD = f"mkdir -p $DIR"
-make_dir = BashOperator(
-    task_id="make_dir",
-    bash_command=CMD,
-    env=ENV_VAR,
-    dag=dag,
-)
+def get_task(task_id, with_dir=True):
+    if with_dir:
+        dir = DIR
+    else:
+        dir = None
 
-tasks = {
+    return BashOperator(
+        task_id=task_id,
+        bash_command=TASKS_CONFIG[task_id],
+        env=ENV_VAR,
+        cwd=dir,
+        dag=dag,
+    )
+
+TASKS_CONFIG = {
+    "mkdir": "mkdir -p $DIR",
     "download_data": "wget $TOLL_DATA_URL -O $TOLL_DATA",
     "unzip_data": "tar zxvf $TOLL_DATA -C $DIR",
     "extract_data_from_csv": "cut -d',' -f1-4 $VEHICLE_DATA > $CSV_DATA",
     "extract_data_from_fixed_width": "cut -c 59-67 $PAYMENT_DATA | tr ' ' ',' > $FIXED_WITH_DATA",
 }
 
-pipeline = make_dir
+pipeline = None
 
-for task_id, cmd in tasks.items():
-    _task = BashOperator(
-        task_id=task_id,
-        bash_command=cmd,
-        env=ENV_VAR,
-        cwd=DIR,
-        dag=dag,
-    )
+for task_id in TASKS_CONFIG.keys():
+    # the first task is mkdir, and need with_dir=false
+    if pipeline is None:
+        pipeline = get_task(task_id, with_dir=false)
+        continue
+
     # pipeline.set_downstream(_task)  # doesn't work
     # pipeline >> _task  # doesn't work
-    pipeline = pipeline >> _task  # this work, but only linear
+    pipeline = pipeline >> get_task(task_id)  # this work, but only linear
